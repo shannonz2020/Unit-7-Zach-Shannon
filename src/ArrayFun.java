79abb876455d1fdb807cb5df9205459771ede75e
shannonz2020 <shannonz2020@gmail.com>
2020-04-27 19:24:42 -0700

    Smol stuff

---
+++ src/ArrayFun.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class ArrayFun {
+
+public Array someArray
+
+    public void arrayMixup(Array arr){
+
+    }
+}
---
+++ src/PoemLine.java
@@ -0,0 +1,15 @@
+public class PoemLine {
+    private String englishLine;
+
+    public PoemLine(String line){
+        englishLine = line;
+    }
+
+    public String getEnglishLine(){
+        return englishLine;
+    }
+
+    /*public String convertToAlienLine(String englishLine){
+
+    }*/
+}
